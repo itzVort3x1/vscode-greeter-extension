9baebaaa8a456c60bce9db5b753813be75fb49a2
itzVort3x1 <saikaustubh10@gmail.com>
2023-02-14 10:04:32 +0530

    updated command

--- extension.js
+++ extension.js
@@ -1,30 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
+const vscode = require("vscode");
+const superheros = require("superheroes");
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-example" is now active!');
-
+	const date = new Date();
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-example.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand(
+		"test-example.searchWDS",
+		function () {
+			// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test-example!');
-	});
+			// Display a message box to the user
+			vscode.window.showInformationMessage(`Hello ${date.getMinutes()}`);
+		}
+	);
 
 	context.subscriptions.push(disposable);
+
+	setTimeout(() => {
+		vscode.window.showInformationMessage(`Hello ${superheros.random()}`);
+	}, 3000);
 }
 
 // This method is called when your extension is deactivated
@@ -32,5 +30,5 @@ function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+};
