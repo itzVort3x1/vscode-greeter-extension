5af79425ffbc89dc2ff5f35e75a5911c0c233453
itzVort3x1 <saikaustubh10@gmail.com>
2023-02-14 11:40:35 +0530

    updated extension

--- extension.js
+++ extension.js
@@ -1,32 +1,78 @@
 const vscode = require("vscode");
 const superheros = require("superheroes");
+const editJsonFile = require("edit-json-file");
+const Quote = require("inspirational-quotes");
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
-	const date = new Date();
+async function activate(context) {
+	let file = editJsonFile(`${__dirname}/data.json`);
+	console.log(Quote.getQuote());
+
+	if (file.get().name === "" || file.get().name === undefined) {
+		const name = await vscode.window.showInputBox({
+			placeHolder: "Enter your name",
+			prompt: "What is your name?",
+		});
+		if (name === "") {
+			vscode.window.showErrorMessage(
+				"Name not entered, use this command sethero to set your name"
+			);
+		} else {
+			file.set("name", name);
+			file.save();
+		}
+	}
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand(
-		"test-example.searchWDS",
-		function () {
+		"test-example.sethero",
+		async function () {
 			// The code you place here will be executed every time your command is executed
 
 			// Display a message box to the user
-			vscode.window.showInformationMessage(`Hello ${date.getMinutes()}`);
+			const name = await vscode.window.showInputBox({
+				placeHolder: "Enter your name",
+				prompt: "What is your name?",
+			});
+			if (name === "") {
+				vscode.window.showErrorMessage(
+					"Name not entered, use this command sethero to set your name"
+				);
+			} else {
+				file.set("name", name);
+				file.save();
+				messager(name);
+			}
 		}
 	);
 
 	context.subscriptions.push(disposable);
 
-	setTimeout(() => {
-		vscode.window.showInformationMessage(`Hello ${superheros.random()}`);
-	}, 3000);
+	if (file.get().name !== "" && file.get().name !== undefined) {
+		messager(file.get().name);
+	}
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+function messager(name) {
+	vscode.window.showInformationMessage(
+		`Welcome back ${name} 🦸 Let's Rock and Roll!`
+	);
+	// vscode.window.showInformationMessage(
+	// 	"Quote of the day",
+	// 	`Welcome back ${name} 🦸 Let's Rock and Roll!`
+	// );
+	setTimeout(() => {
+		vscode.window.showInformationMessage(
+			`Today's hero of the day is: ${superheros.random()}`
+		);
+	}, 5000);
+}
 
 module.exports = {
 	activate,
